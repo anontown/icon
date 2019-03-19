e9b99d5bacfa5f65a5abf5025f3966d1e68bc1b6
kgtkr <kgtkr.jp@gmail.com>
2019-03-19 16:58:56 +0900

    ソース変更

--- build.js
+++ build.js
@@ -3,8 +3,7 @@ const svg2png = require("svg2png");
 
 (async () => {
   const data = await fs.readFile("icon.svg");
-  for (const pow of [4, 5, 6, 7, 8, 9, 10, 11, 12]) {
-    const size = 2 ** pow;
+  for (const size of [4, 5, 6, 7, 8, 9, 10, 11, 12].map(x => 2 ** x)) {
     await fs.writeFile(`${size}.png`, await svg2png(data, { width: size, height: size }));
   }
 })();
